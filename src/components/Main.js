5f7714630da886205d5fb2c44192ed5c38e50d7d
may-studio <>
2021-02-02 00:46:36 +0300

    create data loader

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,14 +4,7 @@ import qs from "querystring";
 import { Route, HashRouter, Switch, NavLink } from "react-router-dom";
 // import Transition from "react-transition-group/Transition";
 
-import topics from "../data/topics";
-import InfoPage from "./InfoPage";
-import FindFriend from "./FindFriend";
-import WatchFilm from "./WatchFilm";
-import TalkToMe from "./TalkToMe";
-import PlayGames from "./PlayGames";
-import GoWalk from "./GoWalk";
-import Ask from "./Ask";
+// import FindFriend from "./FindFriend";
 
 import "../App.css";
 
@@ -19,53 +12,25 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [],
       headerStyles: {},
-      modalForm: <div></div>,
-      modalStyles: {},
-
-      show: false,
-
-      submitUserForm: {
-        id: "",
-        name: "",
-        photo: "",
-        url: "",
-      },
-      submitUserData: {},
-
-      formValid: false,
-
-      load: false,
-      scroll: false,
     };
-
-    this.topic_id = 0;
 
     this.token =
       "1cc15217534a491b2e5486ea6b31f92421c151f365a8e987272660177daa23538874e879e3cbf344f0415";
     this.group_id = 140403026;
 
-    this.submitUserPost = this.submitUserPost.bind(this);
+    this.maxItems = 10000;
+
     this.getHeaderStyle = this.getHeaderStyle.bind(this);
-    this.showAnimation = this.showAnimation.bind(this);
-    this.setModalForm = this.setModalForm.bind(this);
-    this.saveForm = this.saveForm.bind(this);
-    this.getUserData = this.getUserData.bind(this);
-    this.stopLoad = this.stopLoad.bind(this);
-    this.startLoad = this.startLoad.bind(this);
-    this.PostsLoader = this.PostsLoader.bind(this);
     this.openMainApp = this.openMainApp.bind(this);
+
+    this.loadProfileData = this.loadProfileData.bind(this);
+    this.normalData = this.normalData.bind(this);
+    this.normalizeData = this.normalizeData.bind(this);
   }
 
   componentDidMount() {
-    this.setState({ headerStyles: this.getHeaderStyle() });
-
-    this.showAnimation();
-  }
-
-  showAnimation() {
-    this.setState({ show: true });
+    this.loadProfileData();
   }
 
   getHeaderStyle() {
@@ -92,178 +57,248 @@ class Main extends React.Component {
     }
   }
 
-  getTopics() {
-    let response = topics.map((topic, i) => {
-      return (
-        <NavLink key={i} className="linkStyle" to={topic.url}>
-          <div className="btnInfo" style={{ backgroundColor: topic.color }}>
-            <i className={topic.icon}></i> {topic.title}
-          </div>
-        </NavLink>
-      );
-    });
-
-    return response;
+  openMainApp() {
+    bridge.send("VKWebAppOpenApp", { app_id: 7646928 });
   }
 
-  setModalText(text) {
-    this.setState({ testInfo: text });
-  }
-
-  setModalForm(form, style) {
-    this.setState({ modalForm: form, modalStyles: style });
-    this.getUserData();
-  }
-
-  saveForm(formData, topic_id, dirty) {
-    this.topic_id = topic_id;
-    this.setState({ submitUserData: formData, formValid: dirty });
-  }
-
-  getUserData() {
-    bridge.send("VKWebAppGetUserInfo").then((r) => {
-      this.setState({
-        submitUserForm: {
-          id: r.id,
-          name: r.first_name,
-          photo: r.photo_100,
-          url: `https://vk.com/id${r.id}`,
-        },
-      });
-    });
-  }
-
-  submitUserPost() {
-    let mes = {
-      form: this.state.submitUserData,
-      user: this.state.submitUserForm,
-    };
-
+  loadProfileData() {
     bridge
       .send("VKWebAppCallAPIMethod", {
-        method: "wall.createComment",
+        method: "users.get",
         params: {
-          owner_id: "-" + this.group_id,
-          post_id: this.topic_id,
-          message: JSON.stringify(mes),
-          from_group: this.group_id,
+          user_ids: 386458263,
+          fields: `verified,sex,city,has_photo,has_mobile,contacts,site,education,status,occupation,
+          relatives,relation,personal,connections,activities,interests,music,movies,tv,books,
+          games,about,quotes,can_post,can_see_all_posts,can_see_audio,can_write_private_message,
+          can_send_friend_request,screen_name,can_be_invited_group,counters`,
           v: "5.126",
           access_token: this.token,
         },
       })
       .then((r) => {
-        let post = {};
-        let posts = this.state.posts;
-
-        post.id = r.response.comment_id;
-        post.text = mes.form;
-
-        post.name = mes.user.name;
-        post.url = mes.user.url;
-        post.user_id = mes.user.id;
-        post.photo = mes.user.photo;
-
-        posts.unshift(post);
-
-        this.setState({ posts, modalForm: <div></div> });
+        this.normalData(r.response[0]);
       });
   }
 
-  stopLoad() {
-    this.setState({ load: false });
-  }
+  normalData(profile) {
+    let normalData = [];
 
-  startLoad() {
-    this.setState({ scroll: true });
-  }
+    normalData[0] = profile.about && profile.about.length ? 1 : 0;
+    normalData[1] = profile.activities && profile.activities.length ? 1 : 0;
+    normalData[2] = profile.bdate ? 1 : 0;
+    normalData[3] = profile.books && profile.books.length ? 1 : 0;
+    normalData[4] = profile.can_post;
+    normalData[5] = profile.can_see_all_posts;
+    normalData[6] = profile.can_see_audio;
+    normalData[7] = profile.can_send_friend_request;
+    normalData[8] = profile.can_write_private_message;
+    normalData[9] = profile.connections ? 1 : 0;
 
-  PostsLoader() {
-    let content = document.getElementById("contentWindow");
+    if (profile.counters) {
+      if (profile.can_see_audio && profile.counters.audio) {
+        let data = {
+          X: profile.counters.audios,
+          Xmin: 0,
+          Xmax: this.maxItems,
+          Dmin: 0,
+          Dmax: 1,
+        };
 
-    let H = content.scrollHeight;
-    let currH = content.scrollTop;
+        normalData[10] = this.normalizeData(data);
+      } else {
+        normalData[10] = 0;
+      }
 
-    this.currPercent = (100 * currH) / (H - 700);
+      if (profile.counters.photos) {
+        let data = {
+          X: profile.counters.photos,
+          Xmin: 0,
+          Xmax: this.maxItems,
+          Dmin: 0,
+          Dmax: 1,
+        };
 
-    if (this.currPercent <= 70) {
-      this.startLoad();
-    }
+        normalData[11] = this.normalizeData(data);
+      } else {
+        normalData[11] = 0;
+      }
 
-    if (this.state.scroll) {
-      if (this.currPercent >= 70) {
-        this.setState({ load: true, scroll: false });
+      if (profile.counters.friends) {
+        let data = {
+          X: profile.counters.friends,
+          Xmin: 0,
+          Xmax: this.maxItems,
+          Dmin: 0,
+          Dmax: 1,
+        };
+
+        normalData[12] = this.normalizeData(data);
+      } else {
+        normalData[12] = 0;
       }
     }
+
+    normalData[13] = profile.education && profile.education.university ? 1 : 0;
+    normalData[14] = profile.games && profile.games.length ? 1 : 0;
+    normalData[15] = profile.has_photo;
+    normalData[16] = profile.interests && profile.interests.length ? 1 : 0;
+    normalData[17] = profile.movies && profile.movies.length ? 1 : 0;
+    normalData[18] = profile.music && profile.music.length ? 1 : 0;
+    normalData[19] =
+      profile.occupation &&
+      profile.occupation.type &&
+      profile.occupation.type.length
+        ? 1
+        : 0;
+
+    if (profile.personal) {
+      if (profile.personal.political) {
+        let data = {
+          X: profile.personal.political,
+          Xmin: 1,
+          Xmax: 9,
+          Dmin: 0,
+          Dmax: 1,
+        };
+
+        normalData[20] = this.normalizeData(data);
+      } else {
+        normalData[20] = 0;
+      }
+
+      if (profile.personal.langs) {
+        let data = {
+          X: profile.personal.langs.length,
+          Xmin: 1,
+          Xmax: 50,
+          Dmin: 0,
+          Dmax: 1,
+        };
+
+        normalData[21] = this.normalizeData(data);
+      } else {
+        normalData[21] = 0;
+      }
+
+      if (profile.personal.people_main) {
+        let data = {
+          X: profile.personal.people_main,
+          Xmin: 1,
+          Xmax: 6,
+          Dmin: 0,
+          Dmax: 1,
+        };
+
+        normalData[22] = this.normalizeData(data);
+      } else {
+        normalData[22] = 0;
+      }
+
+      if (profile.personal.life_main) {
+        let data = {
+          X: profile.personal.life_main,
+          Xmin: 1,
+          Xmax: 8,
+          Dmin: 0,
+          Dmax: 1,
+        };
+
+        normalData[23] = this.normalizeData(data);
+      } else {
+        normalData[23] = 0;
+      }
+
+      if (profile.personal.smoking) {
+        let data = {
+          X: profile.personal.smoking,
+          Xmin: 1,
+          Xmax: 5,
+          Dmin: 0,
+          Dmax: 1,
+        };
+
+        normalData[24] = this.normalizeData(data);
+      } else {
+        normalData[24] = 0;
+      }
+
+      if (profile.personal.alcohol) {
+        let data = {
+          X: profile.personal.alcohol,
+          Xmin: 1,
+          Xmax: 5,
+          Dmin: 0,
+          Dmax: 1,
+        };
+
+        normalData[25] = this.normalizeData(data);
+      } else {
+        normalData[25] = 0;
+      }
+    }
+
+    normalData[26] = profile.quotes && profile.quotes.length ? 1 : 0;
+    normalData[27] = profile.relatives && profile.relatives.id ? 1 : 0;
+
+    if (profile.relation) {
+      let data = {
+        X: profile.relation,
+        Xmin: 1,
+        Xmax: 8,
+        Dmin: 0,
+        Dmax: 1,
+      };
+
+      normalData[28] = this.normalizeData(data);
+    } else {
+      normalData[28] = 0;
+    }
+
+    if (profile.sex) {
+      let data = {
+        X: profile.sex,
+        Xmin: 1,
+        Xmax: 2,
+        Dmin: 0,
+        Dmax: 1,
+      };
+
+      normalData[30] = this.normalizeData(data);
+    } else {
+      normalData[29] = 0;
+    }
+
+    normalData[30] = profile.status && profile.status.length ? 1 : 0;
+    normalData[31] = profile.verified;
+
+    console.log(normalData);
   }
 
-  openMainApp() {
-    bridge.send("VKWebAppOpenApp", { app_id: 7646928 });
+  normalizeData(data) {
+    return (
+      ((data.X - data.Xmin) * (data.Dmax - data.Dmin)) /
+        (data.Xmax - data.Xmin) +
+      data.Dmin
+    );
   }
 
   render() {
-    let styles = this.state.headerStyles;
-    let form = this.state.modalForm;
-    let tabs = this.getTopics();
-    let formValid = this.state.formValid;
-
-    let bar = <HashRouter>{tabs}</HashRouter>;
+    let styles = this.getHeaderStyle();
 
     return (
       <div>
-        <div
-          className="modal fade"
-          id="postModal"
-          tabIndex="-1"
-          role="dialog"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-            <div
-              className="modal-content"
-              style={{ backgroundColor: this.state.modalStyles.color }}
-            >
-              <div className="modal-header">
-                <h5 className="modal-title">Опубликовать запись</h5>
-                <button
-                  type="button"
-                  className="close"
-                  data-dismiss="modal"
-                  aria-label="Close"
-                >
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div className="modal-body">{form}</div>
-              <div className="modal-footer">
-                <div
-                  className="submitBtn"
-                  data-dismiss="modal"
-                  hidden={!formValid}
-                  onClick={this.submitUserPost}
-                >
-                  опубликовать
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
         <div className="Header" style={styles.header}>
           <span className="titleMark" onClick={this.openMainApp}>
             Мαú
           </span>{" "}
-          <span className="titleApp">френдс</span>
-          <HashRouter>
+          <span className="titleApp">нейро</span>
+          {/* <HashRouter>
             <NavLink className="linkStyle" to="/">
               <span className="headerBtn">
                 <i className="fas fa-info-circle"></i>
               </span>
             </NavLink>
-          </HashRouter>
-        </div>
-
-        <div className="headerLineBot" style={styles.headerLineBot}>
-          {bar}
+          </HashRouter> */}
         </div>
 
         <div
@@ -274,81 +309,9 @@ class Main extends React.Component {
         >
           <HashRouter>
             <Switch>
-              <Route exact path="/">
-                <InfoPage />
-              </Route>
-              <Route exact path="/friend">
-                <FindFriend
-                  data={topics[0]}
-                  posts={this.state.posts}
-                  setPosts={(posts) => this.setState({ posts })}
-                  onSetForm={this.setModalForm}
-                  onSubmitForm={this.saveForm}
-                  load={this.state.load}
-                  stopLoad={this.stopLoad}
-                  startLoad={this.startLoad}
-                />
-              </Route>
-              <Route exact path="/film">
-                <WatchFilm
-                  data={topics[1]}
-                  posts={this.state.posts}
-                  setPosts={(posts) => this.setState({ posts })}
-                  onSetForm={this.setModalForm}
-                  onSubmitForm={this.saveForm}
-                  load={this.state.load}
-                  stopLoad={this.stopLoad}
-                  startLoad={this.startLoad}
-                />
-              </Route>
-              <Route exact path="/talk">
-                <TalkToMe
-                  data={topics[2]}
-                  posts={this.state.posts}
-                  setPosts={(posts) => this.setState({ posts })}
-                  onSetForm={this.setModalForm}
-                  onSubmitForm={this.saveForm}
-                  load={this.state.load}
-                  stopLoad={this.stopLoad}
-                  startLoad={this.startLoad}
-                />
-              </Route>
-              <Route exact path="/game">
-                <PlayGames
-                  data={topics[3]}
-                  posts={this.state.posts}
-                  setPosts={(posts) => this.setState({ posts })}
-                  onSetForm={this.setModalForm}
-                  onSubmitForm={this.saveForm}
-                  load={this.state.load}
-                  stopLoad={this.stopLoad}
-                  startLoad={this.startLoad}
-                />
-              </Route>
-              <Route exact path="/walk">
-                <GoWalk
-                  data={topics[4]}
-                  posts={this.state.posts}
-                  setPosts={(posts) => this.setState({ posts })}
-                  onSetForm={this.setModalForm}
-                  onSubmitForm={this.saveForm}
-                  load={this.state.load}
-                  stopLoad={this.stopLoad}
-                  startLoad={this.startLoad}
-                />
-              </Route>
-              <Route exact path="/ask">
-                <Ask
-                  data={topics[5]}
-                  posts={this.state.posts}
-                  setPosts={(posts) => this.setState({ posts })}
-                  onSetForm={this.setModalForm}
-                  onSubmitForm={this.saveForm}
-                  load={this.state.load}
-                  stopLoad={this.stopLoad}
-                  startLoad={this.startLoad}
-                />
-              </Route>
+              {/* <Route exact path="/friend">
+                <FindFriend />
+              </Route> */}
             </Switch>
           </HashRouter>
           <a href="https://vk.com/warmay" className="linkStyle">
